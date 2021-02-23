adb1b459e4da82d8e3db5d80ce6d0c401992fee7
nigelmsipa <nigelmsipa@gmail.com>
2021-02-22 23:57:12 -0500

    meets expectations

--- js/script.js
+++ js/script.js
@@ -1,33 +1,89 @@
 /** @format */
-
 const gallery = document.querySelector('#gallery');
 
-//Get data from API
+// Used an async await + fetch function + try catch to get the data asyncronously from the API
 async function getData() {
-  const apiUrl = 'https://randomuser.me/api/?results=12&format=Json';
   try {
+    const apiUrl = 'https://randomuser.me/api/?results=12';
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    let dataResults = data.results;
+    data = await response.json();
+    const employees = data.results;
+    employees.forEach(createGalleryAndModal);
 
-    for (let i = 0; i < dataResults.length; i++) {
-      gallery.insertAdjacentHTML(
-        'beforeend',
-        `
-        <div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="${dataResults[i].picture.large}" alt="profile picture">
-        </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${dataResults[i].name.first} ${dataResults[i].name.last}</h3>
-            <p class="card-text">${dataResults[i].email}</p>
-            <p class="card-text cap city">${dataResults[i].location.city}, ${dataResults[i].location.state}</p>
-        </div>`
-      );
+    function createGalleryAndModal(employee, index) {
+      newGallery(employee, index);
+      newModal(employee);
     }
   } catch (error) {
     console.log('something went wrong', error);
   }
 }
-// on Load
 getData();
+
+// dynamicaly creating modal html
+const modal = document.createElement('div');
+modal.className = 'modal-container';
+modal.style.display = 'none';
+document.body.append(modal);
+const modalContainer = document.querySelector('.modal-container');
+
+// function to add employees into the gallery
+function newGallery(data, index) {
+  //card snippet
+  const card = `
+  <div class="card">
+  <div class="card-img-container">
+
+      <img class="card-img" src="${data.picture.large}" alt="profile picture">
+  </div>
+  <div class="card-info-container">
+      <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+      <p class="card-text">${data.email}</p>
+      <p class="card-text cap city">${data.location.city}, ${data.location.state}</p>
+  </div>`;
+
+  gallery.insertAdjacentHTML('beforeend', card);
+
+  const cards = document.querySelectorAll('.card');
+  cards[index].addEventListener('click', (e) => {
+    let modalArray = document.querySelectorAll('.modal');
+    modalArray[index].style.display = '';
+    modalContainer.style.display = '';
+  });
+}
+
+// function to create new modal
+function newModal(data) {
+  $date_regex =
+    '/(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)dd/';
+
+  let birthday = data.dob.date;
+  birthdayformat = birthday.replace($date_regex, '');
+  const modal = `
+    <div class="modal" > 
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${data.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+                <p class="modal-text">${data.email}</p>
+                <p class="modal-text cap">${data.location.city}</p>
+                <hr>
+                <p class="modal-text">${data.phone}</p>
+                <p class="modal-text">${data.location.street.number} ${data.location.street.name} ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+                <p class="modal-text">Birthday: ${birthdayformat}</p>
+            </div>
+            </div>
+        </div>`;
+
+  modalContainer.insertAdjacentHTML('beforeend', modal);
+  const Modal = modalContainer.lastElementChild;
+
+  Modal.style.display = 'none';
+
+  const button = Modal.querySelector('.modal-close-btn');
+
+  button.addEventListener('click', () => {
+    Modal.style.display = 'none';
+    modalContainer.style.display = 'none';
+  });
+}
